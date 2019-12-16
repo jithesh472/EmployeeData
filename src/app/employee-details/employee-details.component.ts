4cbbe2570bff3a30dc666283b03dd4b7fd0436f8
jithesh <jithesh123@gmail.com>
2019-12-16 16:06:20 +0530

    employee detail page design

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -5,10 +5,42 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.scss']
 })
+
 export class EmployeeDetailsComponent implements OnInit {
 
   constructor() { }
-
+  public employeeDetails = [
+    {
+      ProfileImage: '../../assets/download.jpeg',
+      Possition: 'Web Developer',
+      Description: '',
+      FirstName: 'Darshan',
+      MiddleName: 'A',
+      LastName: 'A',
+      Gender: 'Male',
+      PermenentAddress: '#21 , 2nd floor, 3rd Crossroad, Marthahalli, Banglore , Karnataka',
+      CurrentAddress: '#21 , 2nd floor, 3rd Crossroad, Marthahalli, Banglore , Karnataka',
+      City: 'Banglore ',
+      State: 'Karnataka',
+      Country: 'India',
+      Pin: 504458
+    },
+    {
+      ProfileImage: '../../assets/download.jpeg',
+      Possition: 'Web Developer',
+      Description: '',
+      FirstName: 'Darshan',
+      MiddleName: 'A',
+      LastName: 'A',
+      Gender: 'Male',
+      PermenentAddress: '#21 , 2nd floor, 3rd Crossroad, Marthahalli, Banglore , Karnataka',
+      CurrentAddress: '#21 , 2nd floor, 3rd Crossroad, Marthahalli, Banglore , Karnataka',
+      City: 'Banglore ',
+      State: 'Karnataka',
+      Country: 'India',
+      Pin: 504458
+    }
+  ];
   ngOnInit() {
   }
 
